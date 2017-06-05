0374460313a62b7e8e315ff98566b7f5afeb1ca2
Antoine de Torcy <adetorcy@email.unc.edu>
2017-06-05 16:25:39 -0400

    [#74] tickets WIP

--- irods/ticket.py
+++ irods/ticket.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+import random
+import string
 from irods.api_number import api_number
 from irods.message import (
     iRODSMessage, TicketAdminRequest)
@@ -9,17 +11,36 @@ logger = logging.getLogger(__name__)
 
 
 class Ticket(object):
-    def __init__(self, ticket_str=None):
-        self._ticket = ticket_str
+    def __init__(self, session, ticket=None):
+        self._session = session
+        self._ticket = ticket if ticket else self.generate()
+
+    @property
+    def session(self):
+        return self._session
 
     @property
     def ticket(self):
         return self._ticket
 
-    def set_for_session(self, session):
+
+    def generate(self, length=15):
+        return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(length))
+
+
+    def supply(self):
         message_body = TicketAdminRequest("session", self.ticket)
         message = iRODSMessage("RODS_API_REQ", msg=message_body, int_info=api_number['TICKET_ADMIN_AN'])
 
-        with session.pool.get_connection() as conn:
+        with self.session.pool.get_connection() as conn:
+            conn.send(message)
+            response = conn.recv()
+
+
+    def issue(self, permission, target):
+        message_body = TicketAdminRequest("create", self.ticket, permission, target)
+        message = iRODSMessage("RODS_API_REQ", msg=message_body, int_info=api_number['TICKET_ADMIN_AN'])
+
+        with self.session.pool.get_connection() as conn:
             conn.send(message)
             response = conn.recv()
